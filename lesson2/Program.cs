ba948937f3237c87ea53fc17f7e04942f7358782
irina <migalina.i@icloud.com>
2022-09-24 16:20:54 +0300

    lesson2

---
+++ lesson2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число");
+string threeDigitNumber = Console.ReadLine();
+
+if (threeDigitNumber.Length < 3)
+{
+    Console.WriteLine($"В числе {threeDigitNumber} меньше трёх цифр.");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа {threeDigitNumber} - {threeDigitNumber[2]}.");
+}
