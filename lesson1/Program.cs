8ffc88142bdb4bf6e6b08da004dc85d2edbf1d49
irina <migalina.i@icloud.com>
2022-09-24 16:11:03 +0300

    lesson1

---
+++ lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int number = new Random().Next(100, 1000);
+
+string num2 = Convert.ToString(number);
+
+Console.WriteLine(number);
+Console.WriteLine(num2[1]);
